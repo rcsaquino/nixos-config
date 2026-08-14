7d90f074a8b0a19b614be9d3a2209458ea5ebfa5
rcsaquino <rcsaquino.md@gmail.com>
2026-08-14 14:22:28 +0800

    Switch noctalia and noctalia-greeter to use specific nixpkgs inputs and cachix branch
    Add noctalia.cachix.org substituter and trusted public key to flake.nix
    nixConfig

--- flake.nix
+++ flake.nix
@@ -1,17 +1,19 @@
 {
   description = "rcsaquino's NixOS Config";
+
   inputs = {
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    noctalia = {
-      url = "github:noctalia-dev/noctalia";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    noctalia-greeter = {
-      url = "github:noctalia-dev/noctalia-greeter";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    noctalia.url = "github:noctalia-dev/noctalia/cachix";
+    noctalia-greeter.url = "github:noctalia-dev/noctalia-greeter";
     trcc-linux.url = "github:Lexonight1/thermalright-trcc-linux";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
   };
 
   outputs = inputs: {
